24915e4b99320bc26c22929c7f972aba9ebb91b4
jamiepollock <jamie.does.umbraco@gmail.com>
2017-02-22 19:41:45 -0500

    FE implementation of Welcome Screen, no wire up yet

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Jumbotron, Well, Grid, Row, Col, Button } from 'react-bootstrap';
+import WelcomeScreen from './WelcomeScreen';
 
 class App extends Component {
   constructor(props) {
@@ -17,43 +16,9 @@ class App extends Component {
   render() {
     return (
       <main role="main">
-        <Header title={this.state.title} />
-        <WelcomeScreen show={this.state.show.welcomeScreen} />
+        <WelcomeScreen show={this.state.show.welcomeScreen} title={this.state.title} />
         <Game show={this.state.show.game} />
       </main>
-    );
-  }
-}
-
-class Header extends Component {
-  render() {
-    return (
-      <Jumbotron>
-        <Grid>
-          <Row className="show-grid center">
-            <img src={logo} className='App-logo' alt='logo' />
-            <h1>{this.props.title}</h1>
-          </Row>
-        </Grid>
-      </Jumbotron>
-    );
-  }
-}
-
-class WelcomeScreen extends Component {
-  render() {
-    if (!this.props.show) {
-      return null;
-    }
-    return (
-      <Grid>
-        <Row className="show-grid">
-          <Well>
-            <Button bsStyle="primary" bsSize="large" block>Create Room</Button>
-            <Button bsStyle="primary" bsSize="large" block>Join Room</Button>
-          </Well>
-        </Row>
-      </Grid>
     );
   }
 }
---
+++ src/client/src/WelcomeScreen.js
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Jumbotron, Accordion, Panel, Grid, Row, Button, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
+
+class WelcomeScreen extends Component {
+    render() {
+        if (!this.props.show) {
+            return null;
+        }
+        return (
+            <div className="welcome-screen">
+                <Header title={this.props.title} />
+                <Grid>
+                    <Row className="show-grid">
+                        <Accordion>
+                            <Panel header="Create Game" eventKey="c">
+                                <CreateGame />
+                            </Panel>
+                            <Panel header="Join Game" eventKey="j">
+                                <JoinGame />
+                            </Panel>
+                        </Accordion>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <Jumbotron>
+        <Grid>
+          <Row className="show-grid center">
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1>{this.props.title}</h1>
+          </Row>
+        </Grid>
+      </Jumbotron>
+    );
+  }
+}
+
+class CreateGame extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playerName: ''
+        };
+
+        this.handleFormSubmit = this.handlePlayerNameChange.bind(this);
+        this.handlePlayerNameChange = this.handlePlayerNameChange.bind(this);
+    };
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+        //send to socket.io;
+    }
+
+    handlePlayerNameChange(e) {
+        this.setState({ playerName: e.target.value });
+    };
+
+    getPlayerNameValidationState() {
+        if (this.state.errors) {
+            return 'error';
+        }
+    };
+
+    render() {
+        return (
+            <div className='join-game'>
+                <form>
+                    <FormGroup
+                        controlId="form-playername"
+                        validationState={this.getPlayerNameValidationState()}
+                    >
+                        <ControlLabel>Player Name</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.playerName}
+                            placeholder="Enter Name"
+                            onChange={this.handlePlayerNameChange}
+                        />
+                        <FormControl.Feedback />
+                        <HelpBlock>Your name, visible to other players in the game room.</HelpBlock>
+                    </FormGroup>
+                    <Button bsStyle="primary" bsSize="large" block>Create Room</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+
+class JoinGame extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playerName: '',
+            roomId: ''
+        };
+
+        this.handleFormSubmit = this.handlePlayerNameChange.bind(this);
+        this.handlePlayerNameChange = this.handlePlayerNameChange.bind(this);
+        this.handleRoomIdChange = this.handleRoomIdChange.bind(this);
+    };
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+        //send to socket.io;
+    }
+
+    handlePlayerNameChange(e) {
+        this.setState({ playerName: e.target.value });
+    };
+    handleRoomIdChange(e) {
+        this.setState({ roomId: e.target.value });
+    };
+
+    getPlayerNameValidationState() {
+        if (this.state.errors) {
+            return 'error';
+        }
+    };
+    getRoomIdValidationState() {
+        if (this.state.errors) {
+            return 'error';
+        }
+    };
+
+    render() {
+        return (
+            <div className='join-game'>
+                <form>
+                    <FormGroup
+                        controlId="form-playername"
+                        validationState={this.getPlayerNameValidationState()}
+                    >
+                        <ControlLabel>Player Name</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.playerName}
+                            placeholder="Enter Name"
+                            onChange={this.handlePlayerNameChange}
+                        />
+                        <FormControl.Feedback />
+                        <HelpBlock>Your name, visible to other players in the game room.</HelpBlock>
+                    </FormGroup>
+
+                    <FormGroup
+                        controlId="form-playername"
+                        validationState={this.getRoomIdValidationState()}
+                    >
+                        <ControlLabel>Room ID</ControlLabel>
+                        <FormControl
+                            type="text"
+                            value={this.state.roomId}
+                            placeholder="Enter Room ID"
+                            onChange={this.handleRoomIdChange}
+                        />
+                        <FormControl.Feedback />
+                        <HelpBlock>The ID of the room you wish to join.</HelpBlock>
+                    </FormGroup>
+                    <Button bsStyle="primary" bsSize="large" block>Join Room</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default WelcomeScreen;
